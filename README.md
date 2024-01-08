731113b5b830b7e9f3d01650803c06efe17a0ecd
y8nju <89aruc@gmail.com>
2024-01-08 16:07:32 +0900

    remove: reset file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# provider-example
+# flutter_provider
 
-> Flutter(플러터)의 핵심인 state management 방법중 가장 보편적으로 많이 사용되는 provider(프로바이더)의 사용법을 기초부터 확실하게 익혀봅니다.<br>
-> 🔗 [코딩셰프 플러터(Flutter) Provider 강의](https://youtube.com/playlist?list=PLQt_pzi-LLfoVM7n46d3PY1cloRy_gDpX&si=tNPEjS4yV4imTEzf)
+> Flutter(플러터)의 핵심인 state management 방법중 가장 보편적으로 많이 사용되는 provider(프로바이더)의 사용법<br>
+> 🔗 [코딩셰프 플러터(Flutter) Provider 강의](https://youtube.com/playlist?list=PLQt_pzi-LLfoVM7n46d3PY1cloRy_gDpX&si=tNPEjS4yV4imTEzf)<br>
+>  🔗 [Mitch Koko - Provider 📱 Simple State Management • Flutter Tutorial](https://youtu.be/uQuxrZE2dqA?si=6cyyU5oZmkUAvMDh)
--- lib/main.dart
+++ lib/main.dart
@@ -1,300 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:providerexample/models/fish_model.dart';
-import 'package:providerexample/models/seafish_model.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => FishModel(
-            name: 'Salmon',
-            number: 10,
-            size: 'big',
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => SeafishModel(
-            name: 'Tuna',
-            tunaNumber: 0,
-            size: 'middle',
-          ),
-        ),
-      ],
-      child: MaterialApp(
-        home: FishOrder(),
-      ),
-    );
-  }
-}
-
-class FishOrder extends StatelessWidget {
-  const FishOrder({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('Fish Order'),
+          title: const Text('Provider example'),
           centerTitle: true,
         ),
-        body: Center(
+        body: const Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text.rich(
-                TextSpan(
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: 'Fish name: ',
-                    ),
-                    TextSpan(
-                      text: Provider.of<FishModel>(context).name,
-                    ),
-                  ],
+              Text(
+                '1. 코딩셰프',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: 10,
               ),
-              High()
+              Text(
+                'branch: codingchef',
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                '2. Mitch Koko',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'branch: mitchkoko',
+              ),
+              SizedBox(
+                height: 80,
+              ),
             ],
           ),
-        ));
-  }
-}
-
-class High extends StatelessWidget {
-  const High({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'SpicyA is located at high place',
-          style: TextStyle(
-            fontSize: 16,
-          ),
         ),
-        SizedBox(
-          height: 20,
-        ),
-        SpicyA()
-      ],
-    );
-  }
-}
-
-class SpicyA extends StatelessWidget {
-  const SpicyA({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text.rich(
-          TextSpan(
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Fish number: ',
-                ),
-                TextSpan(
-                  text: '${Provider.of<FishModel>(context).number}',
-                )
-              ]),
-        ),
-        Text.rich(
-          TextSpan(
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Fish size: ',
-                ),
-                TextSpan(
-                  text: Provider.of<FishModel>(context).size,
-                )
-              ]),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Middle()
-      ],
-    );
-  }
-}
-
-class Middle extends StatelessWidget {
-  const Middle({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'SpicyB is located at middle place',
-          style: TextStyle(
-            fontSize: 16,
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SpicyB()
-      ],
-    );
-  }
-}
-
-class SpicyB extends StatelessWidget {
-  const SpicyB({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text.rich(
-          TextSpan(
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Tuna number: ',
-                ),
-                TextSpan(
-                  text: '${Provider.of<SeafishModel>(context).tunaNumber}',
-                )
-              ]),
-        ),
-        Text.rich(
-          TextSpan(
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Tuna size: ',
-                ),
-                TextSpan(
-                  text: Provider.of<SeafishModel>(context).size,
-                )
-              ]),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Provider.of<SeafishModel>(
-              context,
-              listen: false,
-            ).changeSeaFishNumber();
-          },
-          child: Text('Change tuna number'),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Low(),
-      ],
-    );
-  }
-}
-
-class Low extends StatelessWidget {
-  const Low({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'SpicyC is located at low place',
-          style: TextStyle(
-            fontSize: 16,
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        SpicyC()
-      ],
-    );
-  }
-}
-
-class SpicyC extends StatelessWidget {
-  const SpicyC({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text.rich(
-          TextSpan(
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Fish number: ',
-                ),
-                TextSpan(
-                  text: '${Provider.of<FishModel>(context).number}',
-                )
-              ]),
-        ),
-        Text.rich(
-          TextSpan(
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Fish size: ',
-                ),
-                TextSpan(
-                  text: Provider.of<FishModel>(context).size,
-                )
-              ]),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Provider.of<FishModel>(context, listen: false).changeFishNumber();
-          },
-          child: Text('Change fish number'),
-        ),
-      ],
+      ),
     );
   }
 }
